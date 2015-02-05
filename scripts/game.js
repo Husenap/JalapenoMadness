86aa09018a1d6111a5173eebcf319bf8de27f117
Husenap <Hussein_t_97@hotmail.com>
2015-02-05 19:25:54 +0100

    WebSocket partly working

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,10 @@
 "use strict";
 
 var ws;
+var connected = false;
+var player;
+var onlinePlayers = Array();
+var players;
 
 requirejs([
 	'./settings'
@@ -14,33 +18,17 @@ requirejs([
 		game.load.tilemap('level1', 'assets/tilemaps/maps/tilemap.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('tiles-1', 'assets/tilemaps/tiles/tiles-1.png');
 	}
-
+	
 	var map;
 	var tileset;
 	var layer;
-	var juan;
 	var speed = 150;
 	var cursors;
 	var jumpButton;
 	var bg;
-
+	
 	function create(){
-
-		ws = new WebSocket("ws://scribblehost.ws:1035/jalamad362250");
-		ws.onopen = function(){
-			alert("Connected!");
-			ws.send("Connected");
-		}
-		ws.onerror = function(error){
-			alert(error);
-		}
-		ws.onmessage = function(e){
-			alert(e.data);
-		}
-		ws.onclose = function(){
-			alert("Disconnected!");
-		}
-
+		
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.physics.arcade.gravity.y = 300;
 
@@ -52,44 +40,85 @@ requirejs([
 		map.setCollisionByExclusion([10, 11, 12, 13, 14, 15, 16, 17, 44, 45, 46, 47, 48, 49, 50, 51]);
 
 		layer = map.createLayer('Tile Layer 1');
-		//layer.debug = true;
 		layer.resizeWorld();
+		players = game.add.group();
+		ws = new WebSocket("ws://scribblehost.ws:1035/jalamad362250");
+		ws.onopen = function(){
+			ws.send(JSON.stringify( {'x': player.x, 'y': player.y} ));
+			connected = true;
+		}
+		ws.onmessage = function(msg){
+			var j = JSON.parse(msg.data);
+			console.log(j);
+			switch(j.id){
+				case 1:
+					onlinePlayers[j.uid] = createPlayerShadow();
+					players.add(onlinePlayers[j.uid]);
+				break;
+				case 2:
+					if(onlinePlayers[j.uid]){
+						onlinePlayers[j.uid].x = j.x;
+						onlinePlayers[j.uid].y = j.y;
+					}else{
+						onlinePlayers[j.uid] = createPlayerShadow();
+						players.add(onlinePlayers[j.uid]);
+						onlinePlayers[j.uid].x = j.x;
+						onlinePlayers[j.uid].y = j.y;
+					}
+				break;
+			}
+		}
+		ws.onerror = function(error){
+			
+		}
+		ws.onclose = function(){
+			alert("Disconnected!");
+		}
 
-		juan = game.add.sprite(32, 32, 'juan');
-		juan.anchor.setTo(0.5, 0.5);
+		player = game.add.sprite(32, 32, 'juan');
+		player.anchor.setTo(0.5, 0.5);
+		game.physics.enable(player, Phaser.Physics.ARCADE);
+		player.body.gravity.y = 982;
+		player.body.maxVelocity.y = 500;
+		player.body.collideWorldBounds = true;
+		player.animations.add('left', [0, 1, 2, 3], 10, true);
+		player.animations.add('right', [5, 6, 7, 8], 10, true);
+		players.add(player);
 
-		game.physics.enable(juan, Phaser.Physics.ARCADE);
-		juan.body.gravity.y = 982;
-		juan.body.maxVelocity.y = 500;
-		juan.body.collideWorldBounds = true;
-		
-		juan.animations.add('left', [0, 1, 2, 3], 60, true);
-		juan.animations.add('right', [5, 6, 7, 8], 60, true);
-
-		game.camera.follow(juan);
+		game.camera.follow(player);
 		cursors = game.input.keyboard.createCursorKeys();
 		jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	}
 	function update(){
 
-		game.physics.arcade.collide(juan, layer);
+		game.physics.arcade.collide(player, layer);
+		game.physics.arcade.collide(players);
 
-		juan.body.velocity.x = 0;
+		player.body.velocity.x = 0;
 		if(cursors.right.isDown){
-			juan.body.velocity.x=speed;
-			juan.animations.play('right');
+			player.body.velocity.x=speed;
+			player.animations.play('right');
 		}else if(cursors.left.isDown){
-			juan.body.velocity.x=-speed;
-			juan.animations.play('left');
+			player.body.velocity.x=-speed;
+			player.animations.play('left');
 		}else{
-			juan.animations.stop();
+			player.animations.stop();
 		}
-		if(jumpButton.isDown && juan.body.onFloor()){
-			juan.body.velocity.y = -500;
+		if(jumpButton.isDown && player.body.onFloor()){
+			player.body.velocity.y = -500;
 		}
+		if(connected)ws.send(JSON.stringify( {'x': player.x, 'y': player.y} ));
 	}
 	function render(){
-	    //game.debug.body(juan);
+	    //game.debug.body(player);
+	}
+	function createPlayerShadow(){
+		var shadow = game.add.sprite(32, 32, 'juan');
+		game.physics.enable(player, Phaser.Physics.ARCADE);
+		shadow.anchor.setTo(0.5, 0.5);
+		shadow.animations.add('left', [0, 1, 2, 3], 10, true);
+		shadow.animations.add('right', [5, 6, 7, 8], 10, true);
+		return shadow;
 	}
 
 });
