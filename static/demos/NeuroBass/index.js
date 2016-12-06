3e9e64ebd17caf1e7950f5a7e20cb98029d2201a
Hussein Taher <hussein_taher@outlook.com>
2016-12-06 21:57:06 +0100

    distortion

--- static/demos/NeuroBass/index.js
+++ static/demos/NeuroBass/index.js
@@ -6,6 +6,11 @@ function Saw(t)
 	return (t%1)*2-1;
 }
 
+function Dist(t, p, e)
+{
+	return 2-Math.exp(-e*(t%(Math.PI/p)));
+}
+
 onload = function update(){
 	requestAnimationFrame(update);
 
@@ -25,6 +30,8 @@ onload = function update(){
 				v  = 0.5*Saw(time * 40);
 				v += 0.3*Saw(time * 41);
 				v += 0.3*Math.sin(time * 40);
+
+				v *= Dist(time, 40, 30);
 
 				str += String.fromCharCode(v*16 + 127);
 			}
