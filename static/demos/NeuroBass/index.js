474b7dde3c2293a23ebdb7c76092cddf88ce4bf1
Hussein Taher <hussein_taher@outlook.com>
2016-12-06 23:59:31 +0100

    AWESOME

--- static/demos/NeuroBass/index.js
+++ static/demos/NeuroBass/index.js
@@ -5,6 +5,16 @@ var audioCtx = new AudioContext();
 var saw1 = audioCtx.createOscillator();
 var saw2 = audioCtx.createOscillator();
 var sine = audioCtx.createOscillator();
+var tri = audioCtx.createOscillator();
+var gain = audioCtx.createGain();
+var lp = audioCtx.createBiquadFilter();
+
+saw1.connect(lp);
+saw2.connect(lp);
+sine.connect(lp);
+tri.connect(gain.gain);
+lp.connect(gain);
+gain.connect(audioCtx.destination);
 
 saw1.type = saw2.type = "sawtooth";
 saw1.frequency.value = 40;
@@ -13,11 +23,21 @@ saw2.frequency.value = 41;
 sine.type = "sine";
 sine.frequency.value = 40;
 
-saw1.connect(audioCtx.destination);
-saw2.connect(audioCtx.destination);
-sine.connect(audioCtx.destination);
+tri.frequency.value = 3;
+
+
 
 saw1.start();
 saw2.start();
 sine.start();
+tri.start();
 
+startTime = Date.now();
+onload = function update()
+{
+	window.requestAnimationFrame(update);
+
+	var t = (Date.now()-startTime)/1000;
+
+
+}
